11cdc8c3da63fad4762fde9cb865ad46cddc2eef
--global <--global>
2022-04-20 18:56:15 +0300

    Change nodemailer configs

--- src/services/mailService.ts
+++ src/services/mailService.ts
@@ -1,20 +1,49 @@
 import { countTotalPrice } from "../helpers/productServiceHelper";
 import { IProductDTO, IUserDTO } from "../interfaces/dtos";
+import { google } from "googleapis";
 import * as nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+
+const createTransporter = async () => {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.OAUTH_CLIENT_ID,
+    process.env.OAUTH_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.OAUTH_REFRESH_TOKEN
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject("Failed to create access token :(");
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: "OAuth2",
+      user: process.env.SMTP_USER,
+      accessToken,
+      clientId: process.env.OAUTH_CLIENT_ID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+  } as SMTPTransport.Options);
+
+  return transporter;
+};
 
 class MailService {
   async sendVerificationMail(to: string, link: string) {
-    console.log(process.env.SMTP_PORT, process.env.SMTP_HOST, process.env.SMTP_USER, process.env.SMTP_PASSWORD, process.env.NODE_ENV, process.env.NODE_ENV == 'production')
+    console.log(process.env.SMTP_PORT, process.env.SMTP_HOST, process.env.SMTP_USER, process.env.SMTP_PASSWORD, process.env.NODE_ENV, process.env.NODE_ENV == 'production', process.env.OAUTH_CLIENT_ID);
 
-    const transporter = nodemailer.createTransport({
-      port: Number(process.env.SMTP_PORT) ?? 0,
-      host: process.env.SMTP_HOST,
-      secure: process.env.NODE_ENV == 'production',
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    });
+    const transporter = await createTransporter();
 
     await transporter.sendMail({
       from: "Course Work",
@@ -36,15 +65,7 @@ class MailService {
     amount?: number,
     paymentMethod?: string
   ) {
-    const transporter = nodemailer.createTransport({
-      port: Number(process.env.SMTP_PORT) ?? 0,
-      host: process.env.SMTP_HOST,
-      secure: false,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    });
+    const transporter = await createTransporter();
 
     await transporter.sendMail({
       from: "Course Work",
